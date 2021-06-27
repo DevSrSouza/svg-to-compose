f0ae36c71c22120d14e4edad8cbe2710c246b914
wiryadev <abrarwiryawan@gmail.com>
2021-06-27 11:49:56 +0700

    refactor Main.kt to improve readability

--- src/test/kotlin/Main.kt
+++ src/test/kotlin/Main.kt
@@ -7,20 +7,22 @@ fun main() {
     val src = File("build/generated-icons").apply { mkdirs()}
 
     Svg2Compose.parse(
-        "br.com.compose.icons",
-        "EvaIcons",
-        src,
-        iconTest,
-        iconNameTransformer = { name, group -> name.removeSuffix(group, ignoreCase = true) }
+        applicationIconPackage = "br.com.compose.icons",
+        accessorName = "EvaIcons",
+        outputSourceDirectory = src,
+        vectorsDirectory = iconTest,
+        iconNameTransformer = { name, group ->
+            name.removeSuffix(group, ignoreCase = true)
+        }
     )
 }
 
 private fun String.removeSuffix(suffix: String, ignoreCase: Boolean): String {
-    if(ignoreCase) {
+    return if(ignoreCase) {
         val index = lastIndexOf(suffix, ignoreCase = true)
 
-        return if(index > -1) substring(0, index) else this
+        if(index > -1) substring(0, index) else this
     } else {
-        return removeSuffix(suffix)
+        removeSuffix(suffix)
     }
 }
