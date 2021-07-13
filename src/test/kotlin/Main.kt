1a67b5b3d3f0ec72cfb570a887e48114917253b1
Abrar Wiryawan <abrarwiryawan@gmail.com>
2021-07-14 04:23:05 +0700

    Update Main.kt (#3)
    * refactor Main.kt to improve readability
    
    * refactor Main.kt to improve readability
    
    * change spacing

--- src/test/kotlin/Main.kt
+++ src/test/kotlin/Main.kt
@@ -4,23 +4,25 @@ import java.io.File
 
 fun main() {
     val iconTest = File("raw-icons4")
-    val src = File("build/generated-icons").apply { mkdirs()}
+    val src = File("build/generated-icons").apply { mkdirs() }
 
     Svg2Compose.parse(
-        "br.com.compose.icons",
-        "EvaIcons",
-        src,
-        iconTest,
-        iconNameTransformer = { name, group -> name.removeSuffix(group, ignoreCase = true) }
+        applicationIconPackage = "br.com.compose.icons",
+        accessorName = "EvaIcons",
+        outputSourceDirectory = src,
+        vectorsDirectory = iconTest,
+        iconNameTransformer = { name, group ->
+            name.removeSuffix(group, ignoreCase = true)
+        }
     )
 }
 
 private fun String.removeSuffix(suffix: String, ignoreCase: Boolean): String {
-    if(ignoreCase) {
+    return if (ignoreCase) {
         val index = lastIndexOf(suffix, ignoreCase = true)
 
-        return if(index > -1) substring(0, index) else this
+        if (index > -1) substring(0, index) else this
     } else {
-        return removeSuffix(suffix)
+        removeSuffix(suffix)
     }
 }
